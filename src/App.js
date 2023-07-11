504dbe434298638cfc2562a3d36ffa17db83da04
Kimsumin123 <tntn211@naver.com>
2023-07-11 13:16:31 +0900

    44

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import './App.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
-  return(
-    <div>
-      <h1>Hello</h1>
-    </div>  
-  )
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Routes>
+        <Route path = "/" exact = {true} Component = {Home}/>
+        <Route path = "/about" Component = {About}/>
+        <Route path = "/movie-detail" Component = {Detail}/>
+      </Routes>
+    </HashRouter>
+  );
 }
-
 export default App;
